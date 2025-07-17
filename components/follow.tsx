217fcbf2c49cdc098ef9d6a74d9be44cefb53853
Arman176001 <armanchaudhary176001@gmail.com>
2025-07-17 09:34:29 +0530

    bug-fix

--- components/follow.tsx
+++ components/follow.tsx
@@ -75,7 +75,7 @@ export default function Follow() {
       opacity: 1,
       y: 0,
       transition: {
-        type: "spring",
+        type: "spring" as const,
         stiffness: 100,
         damping: 10,
       },
